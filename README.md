1fe1c559d3bfef9e3c3fb3d4f8a7f50b39fb31ad
Kei Ito <kei.itof@gmail.com>
2017-02-13 20:43:37 +0900

    update README

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
 # nunjucks-watch
+
+Auto updator for [Nunjucks](https://mozilla.github.io/nunjucks/).
+It resolves location of dependents relative to the rendering target and watch them with [chokidar](https://github.com/paulmillr/chokidar).
+
+# Install
+
+```
+npm install nunjucks-watch
+```
+
+# Usage
+
+## Write result to a file
+
+```javascript
+const nunjucksWatch = require('nunjucks-watch');
+const watcher = nunjucksWatch.watch({
+	src: 'path/to/your/template.nunjucks',
+	dest: 'path/to/dest.html',
+	context: {
+		foo: 'burn'
+	}
+});
+```
+
+## Receive result as a string
+
+```javascript
+const nunjucksWatch = require('nunjucks-watch');
+const watcher = nunjucksWatch.watch({
+	src: 'path/to/your/template.nunjucks',
+	context: {
+		foo: 'burn'
+	}
+})
+	.on('update', function (rendered) {
+		console.log(rendered);
+	});
+```
+
+# API
+
+## nunjucksWatch.watch(options)
+
+Start an updator.
+
+### options
+
+Type: `Object`
+
+### options.src
+
+Type: `String` (required)
+
+A path to a file to be rendered.
+
+### options.dest
+
+Type: `String` (optional)
+
+A path to which an updator will write the rendered text.
+
+### options.context
+
+Type: `Object` (optional)
+
+`options.context` will be used as a context object on [rendering](https://mozilla.github.io/nunjucks/api.html#render).
+### options.environment
+
+Type: `Object` (optional)
+
+`options.environment` will be passed to the [nunjucks.Environment](https://mozilla.github.io/nunjucks/api.html#constructor) constructor.
+
+### options.fsLoader.debounce
+
+Type: `Number` (optional)
+
+Time to wait for debouncing (milliseconds).
---
+++ test/index.js
@@ -0,0 +1,72 @@
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+const chokidar = require('chokidar');
+const debounce = require('../debounce');
+const nunjucksWatch = require('..');
+const currentTime = Date.now();
+const rendered = `<!doctype html>
+<meta charset="utf-8">
+
+<link rel="stylesheet" href="css/index.css">
+
+
+<script src="js/index.js"></script>
+
+<script type="text/plain">${currentTime}</script>
+`;
+
+describe('NunjucksWatcher', function () {
+
+	it('should render a template', function (done) {
+		const targetDir = path.join(__dirname, '001');
+		nunjucksWatch.watch(path.join(targetDir, 'index.nunjucks'), {
+			context: {
+				currentTime: currentTime
+			}
+		})
+			.once('error', done)
+			.once('update', function (result) {
+				assert.equal(result, rendered);
+				this.once('update', () => {
+					done();
+				});
+				fs.utimes(path.join(targetDir, 'layout.nunjucks'), NaN, NaN, (error) => {
+					if (error) {
+						done(error);
+					}
+				});
+			});
+	});
+
+	it('should write the result to dest', function (done) {
+		const targetDir = path.join(__dirname, '001');
+		const destPath = path.join(targetDir, 'output.txt');
+		nunjucksWatch.watch(path.join(targetDir, 'index.nunjucks'), {
+			dest: destPath,
+			context: {
+				currentTime: currentTime
+			}
+		})
+			.once('error', done)
+			.once('update', function () {
+				const watcher = chokidar.watch(destPath);
+				watcher.once('error', function (error) {
+						this.close();
+						done(error);
+					})
+					.on('all', debounce(function () {
+						this.close();
+						fs.readFile(destPath, (error, result) => {
+							if (error) {
+								done(error);
+							} else {
+								assert.equal(result.toString(), rendered);
+								done();
+							}
+						});
+					}, 100, watcher));
+			});
+	});
+
+});
