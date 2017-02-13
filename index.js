ff362959a2890de95b5df6180fa5043da4af9653
Kei Ito <kei.ito@kddi-web.com>
2017-02-13 20:43:28 +0900

    add dest option

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const fs = require('fs');
+const path = require('path');
 const EventEmitter = require('events');
 const nunjucks = require('nunjucks');
+const mkdirp = require('mkdirp');
 
 const FSLoader = require('./FSLoader');
 
@@ -9,23 +12,37 @@ const FSLoader = require('./FSLoader');
 class NunjucksWatcher extends EventEmitter {
 
 	/**
-	 * @param {string} targetFile
-	 * @param {?Object} opts
+	 * @param {Object} opts
+	 * @param {string} opts.src
 	 * @param {?Object} opts.fsLoader
 	 * @param {?Object} opts.environment
 	 * @param {?Object} opts.context
+	 * @param {?string} opts.dest
 	 */
-	constructor (targetFile, opts = {}) {
+	constructor (opts = {}) {
 		super();
 		this.loader = new FSLoader(opts.fsLoader);
 		this.environment = new nunjucks.Environment(this.loader, opts.environment);
 		this.loader
 			.on('update', () => {
-				this.environment.render(targetFile, opts.context, (error, result) => {
+				this.environment.render(opts.src, opts.context, (error, result) => {
 					if (error) {
 						this.emit('error', error);
 					} else {
 						this.emit('update', result);
+						if (opts.dest) {
+							mkdirp(path.dirname(opts.dest), (error) => {
+								if (error) {
+									this.emit('error', error);
+								} else {
+									fs.writeFile(opts.dest, result, (error) => {
+										if (error) {
+											this.emit('error', error);
+										}
+									});
+								}
+							});
+						}
 					}
 				});
 			})
@@ -34,7 +51,7 @@ class NunjucksWatcher extends EventEmitter {
 
 }
 
-exports.watch = (...args) => {
-	return new NunjucksWatcher(...args);
-};
 exports.NunjucksWatcher = NunjucksWatcher;
+exports.watch = (opts) => {
+	return new NunjucksWatcher(opts);
+};
